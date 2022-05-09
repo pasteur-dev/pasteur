09e56cbdabc65f33a4d4ade2d3235727f80f68b7
antheas <antheas@users.noreply.github.com>
2022-05-09 09:51:10 +0000

    cleanup mimic to use only for ingestion

--- src/pasteur/pipeline_registry.py
+++ src/pasteur/pipeline_registry.py
@@ -3,11 +3,7 @@ from typing import Dict
 
 from kedro.pipeline import Pipeline, pipeline
 
-from .pipelines.mimic.pipeline import (
-    create_pipeline as create_pipeline_mimic,
-    create_pipeline_experiment,
-    create_pipeline_ingest,
-)
+from .pipelines.mimic.pipeline import create_pipeline as create_pipeline_mimic
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -17,13 +13,11 @@ def register_pipelines() -> Dict[str, Pipeline]:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
 
-    mimic_pipeline = create_pipeline_mimic()
-    mimic_ingest_pipeline = create_pipeline_ingest()
-    mimic_experiment_pipeline = create_pipeline_experiment()
+    mimic_pipeline = create_pipeline_mimic(set(["mimic.hosp_d_hcpcs"]))
+    mimic_ingest_pipeline = create_pipeline_mimic()
 
     return {
         "__default__": mimic_pipeline,
         "mimic": mimic_pipeline,
-        "mimic_ingest": mimic_ingest_pipeline,
-        "mimic_experiment": mimic_experiment_pipeline,
+        "mimic_ingest_pipeline": mimic_ingest_pipeline,
     }
--- src/pasteur/pipelines/mimic/pipeline.py
+++ src/pasteur/pipelines/mimic/pipeline.py
@@ -6,8 +6,6 @@ from typing import Optional, Set
 from kedro.pipeline import Pipeline, node, pipeline
 from kedro.pipeline.modular_pipeline import pipeline as modular_pipeline
 
-from pasteur.pipelines.mimic.nodes import select_id
-
 from ..general.pipeline import create_node_split_keys
 from ..general.nodes import identity
 
@@ -43,14 +41,18 @@ def create_intermediate_data(inputs: Optional[Set] = None) -> Pipeline:
         "icu_procedureevents",
     ]
 
-    mimic_tables = inputs.intersection(mimic_tables_all) if inputs else mimic_tables_all
+    if inputs:
+        inputs_clear = {t.replace("mimic.", "").split("@")[0] for t in inputs}
+        mimic_tables = inputs_clear.intersection(mimic_tables_all)
+    else:
+        mimic_tables = mimic_tables_all
 
     parquet_pipeline = pipeline(
         [
             node(
                 func=identity,
                 inputs=["mimic_iv@%s" % t],
-                outputs=t,
+                outputs="%s@all" % t,
                 name="ingest_%s" % t,
             )
             for t in mimic_tables
@@ -59,39 +61,19 @@ def create_intermediate_data(inputs: Optional[Set] = None) -> Pipeline:
     return parquet_pipeline
 
 
-def create_pipeline_split_mimic_keys(**kwargs) -> Pipeline:
-    return pipeline(
-        [
-            node(select_id, inputs=["core_patients"], outputs="keys_all"),
-            create_node_split_keys(),
-        ],
+def create_pipeline_split_mimic_keys() -> Pipeline:
+    return modular_pipeline(
+        pipeline([create_node_split_keys()]), inputs={"keys_all": "core_patients@keys"}
     )
 
 
-def create_pipeline_mimic_tasks(**kwargs) -> Pipeline:
-    return create_pipeline_split_mimic_keys()
-
-
-def create_pipeline_ingest():
-    return modular_pipeline(
-        create_intermediate_data(), namespace="mimic", inputs=["mimic_iv"]
-    )
-
-
-def create_pipeline_experiment():
-    return modular_pipeline(
-        create_pipeline_mimic_tasks(),
-        namespace="mimic",
-        parameters=["random_state"],
-    )
-
-
-def create_pipeline(**kwargs) -> Pipeline:
-    mimic_pipeline = create_pipeline_split_mimic_keys()
-    mimic_inputs = create_intermediate_data(mimic_pipeline.inputs())
+def create_pipeline(inputs: Optional[Set] = None) -> Pipeline:
+    mimic_keys = create_pipeline_split_mimic_keys()
+    inputs = inputs.union(mimic_keys.inputs()) if inputs is not None else None
+    mimic_ingest = create_intermediate_data(inputs)
 
     return modular_pipeline(
-        mimic_pipeline + mimic_inputs,
+        mimic_keys + mimic_ingest,
         namespace="mimic",
         inputs=["mimic_iv"],
         parameters=["random_state"],
