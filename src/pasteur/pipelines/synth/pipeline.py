78eeccf5dc7c4e132590451207467a8bb341a4bf
antheas <antheas@users.noreply.github.com>
2022-05-21 08:58:19 +0000

    tweak synth pipelines

--- src/pasteur/pipelines/synth/pipeline.py
+++ src/pasteur/pipelines/synth/pipeline.py
@@ -11,27 +11,27 @@ from pasteur.pipelines.synth.nodes import (
 
 def create_transform_pipeline(tables):
     tables = [t.split(".")[-1] for t in tables]
-    orig_tables = {t: "orig:primary:%s" % t for t in tables}
-    encoded_tables = {t: "orig:encoded:%s" % t for t in tables}
-    decoded_tables = {t: "orig:decoded:%s" % t for t in tables}
 
     table_nodes = []
     for t in tables:
         table_nodes += [
             node(
                 func=fit_table,
-                inputs=["view:primary:%s" % t, "params:metadata.tables.%s" % t],
-                outputs="view:transformer:%s" % t,
+                inputs=["primary.%s" % t, "params:metadata.tables.%s" % t],
+                outputs="transformer.%s" % t,
+                namespace="transformer",
             ),
             node(
                 func=transform_table,
-                inputs=["view:primary:%s" % t, "view:transformer:%s" % t],
-                outputs="view:encoded:%s" % t,
+                inputs=["primary.%s" % t, "transformer.%s" % t],
+                outputs="encoded.%s" % t,
+                namespace="encoded",
             ),
             node(
                 func=reverse_transform_table,
-                inputs=["view:encoded:%s" % t, "view:transformer:%s" % t],
-                outputs="view:decoded:%s" % t,
+                inputs=["encoded.%s" % t, "transformer.%s" % t],
+                outputs="decoded.%s" % t,
+                namespace="decoded",
             ),
         ]
 
@@ -39,7 +39,7 @@ def create_transform_pipeline(tables):
 
 
 def create_pipeline(dataset: str, tables: Collection[str]) -> Pipeline:
-    table_mapping = {"view:primary:%s" % t.split(".")[-1]: t for t in tables}
+    table_mapping = {"primary.%s" % t.split(".")[-1]: t for t in tables}
     parameters = {
         "params:metadata.tables.%s"
         % t.split(".")[-1]: "params:%s.metadata.tables.%s"
@@ -49,7 +49,7 @@ def create_pipeline(dataset: str, tables: Collection[str]) -> Pipeline:
 
     return modular_pipeline(
         pipe=create_transform_pipeline(tables),
-        namespace="synth",
+        namespace="view",
         inputs=table_mapping,
         parameters=parameters,
     )
