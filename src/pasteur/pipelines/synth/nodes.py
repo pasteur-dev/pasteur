a8de6886fadd9f52c3bdae8c3eaa9bd8a33bcc42
antheas <antheas@users.noreply.github.com>
2022-05-23 22:02:45 +0000

    add nonworking synth pipeline

--- src/pasteur/pipelines/synth/nodes.py
+++ src/pasteur/pipelines/synth/nodes.py
@@ -1,7 +1,13 @@
 from __future__ import absolute_import
+from curses import meta
+from importlib.metadata import metadata
+from statistics import median
 from typing import Dict
 
 from rdt import HyperTransformer
+from sdv.relational import HMA1
+from sdv.relational.base import BaseRelationalModel
+from sdv.metadata import Metadata
 
 import pandas as pd
 
@@ -20,3 +26,17 @@ def transform_table(table: pd.DataFrame, transformer: HyperTransformer):
 
 def reverse_transform_table(table: pd.DataFrame, transformer: HyperTransformer):
     return transformer.reverse_transform(table)
+
+
+def sdv_fit(metadata: Dict, **kwargs: pd.DataFrame):
+    tables = kwargs
+    metadata = Metadata(metadata)
+
+    model = HMA1(metadata)
+    model.fit(tables)
+
+    return model
+
+
+def sdv_sample(model: BaseRelationalModel):
+    return model.sample()
--- src/pasteur/pipelines/synth/pipeline.py
+++ src/pasteur/pipelines/synth/pipeline.py
@@ -1,9 +1,12 @@
 from typing import Collection
 from kedro.pipeline import Pipeline, node, pipeline
 from kedro.pipeline.modular_pipeline import pipeline as modular_pipeline
+from pasteur.pipelines.general.nodes import filter_by_keys
 
 from pasteur.pipelines.synth.nodes import (
     fit_table,
+    sdv_fit,
+    sdv_sample,
     transform_table,
     reverse_transform_table,
 )
@@ -17,12 +20,12 @@ def create_transform_pipeline(tables):
         table_nodes += [
             node(
                 func=fit_table,
-                inputs=[t, "params:metadata.tables.%s" % t],
+                inputs=["wrk_%s" % t, "params:metadata.tables.%s" % t],
                 outputs="transformer_%s" % t,
             ),
             node(
                 func=transform_table,
-                inputs=[t, "transformer_%s" % t],
+                inputs=["wrk_%s" % t, "transformer_%s" % t],
                 outputs="encoded_%s" % t,
             ),
             node(
@@ -35,16 +38,62 @@ def create_transform_pipeline(tables):
     return pipeline(table_nodes)
 
 
-def create_pipeline(dataset: str, tables: Collection[str]) -> Pipeline:
-    parameters = {
-        "params:metadata.tables.%s"
-        % t.split(".")[-1]: "params:%s.metadata.tables.%s"
-        % (dataset, t.split(".")[-1])
-        for t in tables
-    }
+def create_synth_pipeline(tables):
+    tables = [t.split(".")[-1] for t in tables]
+    alg = "hma1"
 
+    synth_pipe = pipeline(
+        [
+            node(
+                func=sdv_fit,
+                inputs={
+                    "metadata": "params:metadata",
+                    **{t: "encoded_%s" % t for t in tables},
+                },
+                outputs="%s_model" % alg,
+            ),
+            node(
+                func=sdv_sample,
+                inputs="%s_model" % alg,
+                outputs=["%s_encoded_%s" % (alg, t) for t in tables],
+            ),
+        ]
+    )
+
+    decode_pipe = pipeline(
+        [
+            node(
+                func=reverse_transform_table,
+                inputs=["%s_encoded_%s" % (alg, t), "transformer_%s" % t],
+                outputs="%s_%s" % (alg, t),
+            )
+            for t in tables
+        ]
+    )
+
+    return synth_pipe + decode_pipe
+
+
+def create_split_pipeline(tables):
+    tables = [t.split(".")[-1] for t in tables]
+
+    return pipeline(
+        [
+            node(
+                func=filter_by_keys,
+                inputs=["%s" % t, "keys_wrk"],
+                outputs="wrk_%s" % t,
+            )
+            for t in tables
+        ]
+    )
+
+
+def create_pipeline(dataset: str, view: str, tables: Collection[str]) -> Pipeline:
     return modular_pipeline(
-        pipe=create_transform_pipeline(tables),
-        namespace=dataset,
-        parameters=parameters,
+        pipe=create_transform_pipeline(tables)
+        + create_synth_pipeline(tables)
+        + create_split_pipeline(tables),
+        namespace=view,
+        inputs={"keys_wrk": "%s.keys_wrk" % dataset},
     )
