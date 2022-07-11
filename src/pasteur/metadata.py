e2e68c66e97d30d28e6ffe6922112d347c1a4488
antheas <antheas@users.noreply.github.com>
2022-07-11 17:43:28 +0000

    start analyzing adult set

---
+++ src/pasteur/metadata.py
@@ -0,0 +1,71 @@
+from typing import Dict, Optional
+import pandas as pd
+
+
+class ColumnMeta:
+    def __init__(self, type, dtype, *args, **kwargs):
+        self.type = type
+        self.dtype = dtype
+
+    def is_categorical(self):
+        return self.type == "categorical"
+
+    def is_cat(self):
+        return self.is_categorical()
+
+    def is_id(self):
+        return self.type == "id"
+
+
+class TableMeta:
+    def __init__(self, meta: Dict, data: Optional[pd.DataFrame] = None):
+        self.primary_key = meta["primary_key"]
+        self._columns = {}
+
+        # Run a key check to ensure metadata and table have the same keys
+        if data is not None:
+            table_keys = set(data.keys())
+            meta_keys = set(meta["fields"].keys())
+
+            diff_keys = table_keys.difference(meta_keys)
+            assert not diff_keys, "Columns missing from table/metadata: " + str(
+                diff_keys
+            )
+
+        fields = meta["fields"]
+        for name, field in fields.items():
+            dtype = str(field.dtypes["name"]) if data is not None else None
+
+            if isinstance(field, str):
+                type = field
+            else:
+                type = field["type"]
+                dtype = field.get("dtype", dtype)
+
+            self._columns[name] = ColumnMeta(type, dtype)
+
+    @property
+    def columns(self):
+        return list(self._columns.keys())
+
+    @property
+    def cols(self):
+        return self.columns
+
+    def __getitem__(self, col):
+        return self._columns[col]
+
+
+class DatasetMeta:
+    def __init__(self, meta: Dict, data: Optional[Dict[str, pd.DataFrame]] = None):
+        self.tables = {
+            name: TableMeta(tmeta, data.get(name, None) if data is not None else None)
+            for name, tmeta in meta["tables"].items()
+        }
+
+    def get_table(self, name):
+        return self.tables[name]
+
+
+class Metadata(DatasetMeta):
+    pass
