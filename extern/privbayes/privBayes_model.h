757c1b428f26b256a681c2e643ccb118fd4c76d8
antheas <antheas@users.noreply.github.com>
2022-08-05 17:32:42 +0000

    remove c privbayes

--- extern/privbayes/privBayes_model.h
+++
@@ -1,5 +0,0 @@
-#include <vector>
-#include <iostream>
-using namespace std;
-
-string c_get_model(const int *data, int m, int n, const string &config, double e1, double e2, double theta, int seed);
