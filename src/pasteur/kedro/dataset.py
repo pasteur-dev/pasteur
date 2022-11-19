c40b4fa27c66efed8cff183e53545f9324c93bfc
antheas <antheas@users.noreply.github.com>
2022-11-19 17:08:21 +0000

    switch to partitioned dataset

---
+++ src/pasteur/kedro/dataset.py
@@ -0,0 +1,52 @@
+from kedro.io.partitioned_dataset import PartitionedDataSet
+import re
+
+
+class PatternDataSet(PartitionedDataSet):
+    """Adds pattern support to Partitioned Dataset"""
+
+    def __init__(
+        self,
+        path: str,
+        dataset,
+        filepath_arg: str = "filepath",
+        filename_suffix: str = "",
+        credentials=None,
+        load_args=None,
+        fs_args=None,
+        overwrite: bool = False,
+        pattern: str = "",
+        replace_pattern: str = "",
+        replace_format: str = "",
+    ):
+        super().__init__(
+            path,
+            dataset,
+            filepath_arg,
+            filename_suffix,
+            credentials,  # type: ignore
+            load_args,  # type: ignore
+            fs_args,  # type: ignore
+            overwrite,
+        )
+        self._pattern = pattern
+        self._replace_pattern = replace_pattern
+        self._replace_format = replace_format
+
+    def _list_partitions(self):
+        if not self._pattern:
+            return super()._list_partitions()
+        return list(
+            filter(lambda p: re.search(self._pattern, p), super()._list_partitions())
+        )
+
+    def _path_to_partition(self, path: str) -> str:
+        id = super()._path_to_partition(path)
+        if not self._replace_pattern or not self._replace_format:
+            return id
+
+        m = re.search(self._replace_pattern, path)
+        if not m:
+            return id
+
+        return self._replace_format.format(*m.groups(), **m.groupdict())
