f16fbcae7fb796ac8da573ceeaee516b6ab470ca
antheas <antheas@users.noreply.github.com>
2023-11-06 22:05:07 +0000

    add condensed mode for cleaner presentation

--- src/pasteur/graph/utils.py
+++ src/pasteur/graph/utils.py
@@ -18,14 +18,14 @@ def display_pydot(g, prog="dot", graph={}, nodes={}, edges={}):
     display(SVG(g.create(format="svg", prog=[prog, *args])))
 
 
-def enchanced_display(g):
+def enchanced_display(g, condensed=True):
     import pydot
 
     if g.is_directed():
         graph_type = "digraph"
     else:
         graph_type = "graph"
-    strict = nx.number_of_selfloops(g) == 0 and not g.is_multigraph()
+    strict = nx.number_of_selfloops(g) == 0 and not g.is_multigraph() and not condensed
 
     graph_defaults = g.graph.get("graph", {})
     o = pydot.Dot(g.name, graph_type=graph_type, strict=strict, **graph_defaults)
@@ -33,12 +33,17 @@ def enchanced_display(g):
     # Keep how many nodes each attribute has for a cleaner appearance
     attr_counts = defaultdict(int)
     attr_vals = defaultdict(set)
+    marked_vals = defaultdict(lambda: False)
     for node_name, d in g.nodes(data=True):
         attr_counts[(d["table"], d["order"], d["attr"])] += 1
         attr_vals[(d["table"], d["order"], d["attr"])].add(d["value"])
+        marked_vals[(d["table"], d["order"], d["attr"], d["value"])] |= d.get(
+            "marked", False
+        )
 
     table_subs: dict[tuple, pydot.Graph] = {}
     attr_subs: dict[tuple, pydot.Graph] = {}
+    val_names: dict[tuple, str] = {}  # pick random name for condensed node
 
     for node_name, d in g.nodes(data=True):
         # Set up clusters
@@ -61,30 +66,41 @@ def enchanced_display(g):
         if (d["table"], d["order"], d["attr"]) not in attr_subs:
             name = d["attr"]
 
-            if attr_counts[(d["table"], d["order"], d["attr"])] > 1:
-                sub = pydot.Cluster(name, label=d["attr"])
-            else:
+            if attr_counts[(d["table"], d["order"], d["attr"])] <= 1 or (
+                condensed and len(attr_vals[d["table"], d["order"], d["attr"]]) == 1
+            ):
                 sub = pydot.Subgraph(name, label="")
+            else:
+                sub = pydot.Cluster(name, label=d["attr"])
             attr_subs[(d["table"], d["order"], d["attr"])] = sub
             table_subs[(d["table"], d["order"])].add_subgraph(sub)
 
         # Setup attribute
         if len(attr_vals[d["table"], d["order"], d["attr"]]) == 1:
-            if attr_counts[(d["table"], d["order"], d["attr"])] > 1:
+            if attr_counts[(d["table"], d["order"], d["attr"])] > 1 and not condensed:
                 label = ""
             else:
                 label = d["value"]
         else:
             label = d["value"].replace(d["attr"] + "_", "")
 
-        label += f"[{d['height']}]"
+        if not condensed:
+            label += f"[{d['height']}]"
         new_data = {"label": label}
 
-        if d.get("marked", False):
+        if d.get("marked", False) or (
+            condensed and marked_vals[d["table"], d["order"], d["attr"], d["value"]]
+        ):
             new_data["color"] = "green"
-        attr_subs[(d["table"], d["order"], d["attr"])].add_node(
-            pydot.Node(node_name, **new_data)
-        )
+
+        if (
+            not condensed
+            or (d["table"], d["order"], d["attr"], d["value"]) not in val_names
+        ):
+            val_names[(d["table"], d["order"], d["attr"], d["value"])] = node_name
+            attr_subs[(d["table"], d["order"], d["attr"])].add_node(
+                pydot.Node(node_name, **new_data)
+            )
 
     for a, b, data in g.edges(data=True):
         new_data = {}
@@ -106,6 +122,33 @@ def enchanced_display(g):
             dst = attr_subs[
                 (g.nodes[a]["table"], g.nodes[a]["order"], g.nodes[a]["attr"])
             ]
+
+        if condensed:
+            if ah := g.nodes[a]["height"]:
+                new_data["taillabel"] = str(ah)
+            if bh := g.nodes[b]["height"]:
+                new_data["headlabel"] = str(bh)
+
+            a = val_names[
+                (
+                    g.nodes[a]["table"],
+                    g.nodes[a]["order"],
+                    g.nodes[a]["attr"],
+                    g.nodes[a]["value"],
+                )
+            ]
+            b = val_names[
+                (
+                    g.nodes[b]["table"],
+                    g.nodes[b]["order"],
+                    g.nodes[b]["attr"],
+                    g.nodes[b]["value"],
+                )
+            ]
+            if a == b:
+                # Do not add edge to self
+                continue
+
         dst.add_edge(pydot.Edge(a, b, **new_data))
 
     display_pydot(o, edges={"labeldistance": 1.5})
