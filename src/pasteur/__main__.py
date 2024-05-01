947caea59d82006f4a45d106a8b394bec45f3b95
antheas <antheas@users.noreply.github.com>
2024-05-01 18:47:34 +0000

    override logging config

--- src/pasteur/__main__.py
+++ src/pasteur/__main__.py
@@ -1,8 +1,21 @@
 """Pasteur file for ensuring the package is executable
 as `pasteur` and `python -m pasteur`
 """
- 
-from kedro.framework.cli import main
+
+
+def main():
+    import os
+
+    print(
+        f"Overriding kedro config with '{os.path.join(os.curdir, 'conf/base/logging.yml')}'"
+    )
+    os.environ["KEDRO_LOGGING_CONFIG"] = os.path.join(
+        os.curdir, "conf/base/logging.yml"
+    )
+    from kedro.framework.cli import main as kedro_main
+
+    return kedro_main()
+
 
 if __name__ == "__main__":
     main()
