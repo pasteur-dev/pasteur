9dcd9bf21f050a22c8c223b85d8f6c61ca7da36e
antheas <antheas@users.noreply.github.com>
2024-05-27 11:02:04 +0000

    prevent crash if logging is missing

--- src/pasteur/__main__.py
+++ src/pasteur/__main__.py
@@ -6,12 +6,10 @@ as `pasteur` and `python -m pasteur`
 def main():
     import os
 
-    print(
-        f"Overriding kedro config with '{os.path.join(os.curdir, 'conf/base/logging.yml')}'"
-    )
-    os.environ["KEDRO_LOGGING_CONFIG"] = os.path.join(
-        os.curdir, "conf/base/logging.yml"
-    )
+    log_fn = os.path.join(os.curdir, "conf/base/logging.yml")
+    if os.path.exists(log_fn):
+        print(f"Overriding kedro config with '{log_fn}'")
+        os.environ["KEDRO_LOGGING_CONFIG"] = log_fn
     from kedro.framework.cli import main as kedro_main
 
     return kedro_main()
