ecef96df44f5dfe4d60c133b93f20bb1503f089d
antheas <antheas@users.noreply.github.com>
2024-05-27 20:53:35 +0200

    fix multiset versioning

--- src/pasteur/kedro/dataset/multi.py
+++ src/pasteur/kedro/dataset/multi.py
@@ -1,22 +1,22 @@
 import warnings
 from copy import deepcopy
+from pathlib import PurePosixPath
 from typing import Any, Callable
+from urllib.parse import urlparse
 
 from kedro.io.core import (
     VERSION_KEY,
     VERSIONED_FLAG_KEY,
     AbstractDataset,
+    AbstractVersionedDataset,
     DatasetError,
     parse_dataset_definition,
 )
 
-
-from urllib.parse import urlparse
-
 S3_PROTOCOLS = ("s3", "s3a", "s3n")
 
 
-class Multiset(AbstractDataset):
+class Multiset(AbstractVersionedDataset):
     # noqa: too-many-instance-attributes,protected-access
     """Simplified version of the partitioned dataset. Is not lazy."""
 
@@ -34,7 +34,6 @@ class Multiset(AbstractDataset):
         # noqa: import-outside-toplevel
         from fsspec.utils import infer_storage_options  # for performance reasons
 
-        super().__init__()
 
         self._path = path
         self._filename_suffix = filename_suffix
@@ -44,9 +43,7 @@ class Multiset(AbstractDataset):
         dataset = dataset if isinstance(dataset, dict) else {"type": dataset}
 
         self._dataset_type, self._dataset_config = parse_dataset_definition(
-            dataset,
-            load_version=version.load if version else None,
-            save_version=version.save if version else None,
+            dataset
         )
 
         self._credentials = deepcopy(credentials) or {}
@@ -62,6 +59,13 @@ class Multiset(AbstractDataset):
         # since some filesystem implementations may implement a global cache
         self._invalidate_caches()
 
+        super().__init__(
+            filepath=PurePosixPath(path),  # type: ignore
+            version=version,
+            exists_function=self._filesystem.exists,
+            glob_function=self._filesystem.glob,
+        )
+
     @property
     def _filesystem(self):
         # for performance reasons
@@ -76,15 +80,32 @@ class Multiset(AbstractDataset):
             return urlparse(self._path)._replace(scheme="s3").geturl()
         return self._path
 
+    def _get_save_path(self):
+        if not self._version:
+            # When versioning is disabled, return original filepath
+            return self._filepath
+
+        save_version = self.resolve_save_version()
+        versioned_path = self._get_versioned_path(save_version)  # type: ignore
+
+        # TODO; Redo check that respects partitioning
+        # if self._exists_function(str(versioned_path)):
+        #     raise DatasetError(
+        #         f"Save path '{versioned_path}' for {str(self)} must not exist if "
+        #         f"versioning is enabled."
+        #     )
+
+        return versioned_path
+
     def _list_partitions(self) -> list[str]:
-        if not self._filesystem.isdir(self._normalized_path, **self._load_args):
+        if not self._filesystem.isdir(self._get_load_path(), **self._load_args):
             # If the path does not exist, ie no datasets were saved before
             # return no partitions instead of crashing
             return []
         return [
             path["name"]
             for path in self._filesystem.listdir(
-                self._normalized_path, **self._load_args
+                self._get_load_path(), **self._load_args
             )
             if path["name"].endswith(self._filename_suffix)
         ]
@@ -97,14 +118,16 @@ class Multiset(AbstractDataset):
             return f"{protocol_prefix}{path}"
         return path
 
-    def _partition_to_path(self, path: str):
-        dir_path = self._path.rstrip(self._sep)
+    def _partition_to_path(self, path: str, load: bool):
+        dir_path = self._get_load_path() if load else self._get_save_path()
+        dir_path = str(dir_path).rstrip(self._sep)
         path = path.lstrip(self._sep)
         full_path = self._sep.join([dir_path, path]) + self._filename_suffix
         return full_path
 
-    def _path_to_partition(self, path: str) -> str:
-        dir_path = self._filesystem._strip_protocol(self._normalized_path)
+    def _path_to_partition(self, path: str, load: bool) -> str:
+        dir_path = self._get_load_path() if load else self._get_save_path()
+        dir_path = self._filesystem._strip_protocol(dir_path)
         path = path.split(dir_path, 1).pop().lstrip(self._sep)
         if self._filename_suffix and path.endswith(self._filename_suffix):
             path = path[: -len(self._filename_suffix)]
@@ -118,7 +141,7 @@ class Multiset(AbstractDataset):
             # join the protocol back since PySpark may rely on it
             kwargs[self._filepath_arg] = self._join_protocol(partition)
             dataset = self._dataset_type(**kwargs)  # type: ignore
-            partition_id = self._path_to_partition(partition)
+            partition_id = self._path_to_partition(partition, load=True)
             partitions[partition_id] = dataset.load()
 
         return partitions
@@ -126,7 +149,7 @@ class Multiset(AbstractDataset):
     def _save(self, data: dict[str, Any]) -> None:
         for partition_id, partition_data in sorted(data.items()):
             kwargs = deepcopy(self._dataset_config)
-            partition = self._partition_to_path(partition_id)
+            partition = self._partition_to_path(partition_id, load=False)
             # join the protocol back since tools like PySpark may rely on it
             kwargs[self._filepath_arg] = self._join_protocol(partition)
             dataset = self._dataset_type(**kwargs)  # type: ignore
@@ -153,8 +176,8 @@ class Multiset(AbstractDataset):
 
     def reset(self):
         """Removes the dataset from disk so that there are no stray partitions in subsequent runs."""
-        if self._filesystem.exists(self._normalized_path):
-            self._filesystem.rm(self._normalized_path, recursive=True, maxdepth=1)
+        if self._filesystem.exists(self._get_save_path()):
+            self._filesystem.rm(self._get_save_path(), recursive=True, maxdepth=1)
 
     def _release(self) -> None:
         super()._release()
