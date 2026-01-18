49648ab231c5390518e266b468a20fe7fe6c2db4
Antheas Kapenekakis <git@antheas.dev>
2026-01-18 18:13:26 +0000

    fix ID handling for rfel datasets
    Signed-off-by: Antheas Kapenekakis <git@antheas.dev>

--- src/pasteur/extras/datasets/rfel/__init__.py
+++ src/pasteur/extras/datasets/rfel/__init__.py
@@ -20,13 +20,20 @@ rfel = "See https://relational.fel.cvut.cz/about for citation and license info."
 class RfelDataset(Dataset):
 
     def __init__(
-        self, short_name: str, name: str, db: str, tables: dict[str, list[str]], **kwargs
+        self,
+        short_name: str,
+        name: str,
+        db: str,
+        tables: dict[str, list[str]],
+        keys: None | str | dict[str, str | list[str]] = None,
+        **kwargs,
     ) -> None:
         from itertools import chain
 
         self.name = f"rfel_{short_name}"
         self.key_deps = [next(iter(tables))]
         self.deps = tables
+        self._keys = keys
 
         self.folder_name = "rfel/" + name
         self.catalog = {
@@ -42,14 +49,33 @@ class RfelDataset(Dataset):
         )
         super().__init__(**kwargs)
 
-    def _process_chunk(self, tables: dict[str, "pd.DataFrame"]):
+    def _process_chunk(self, name, tables: dict[str, "pd.DataFrame"]):
         assert len(tables) == 1
-        return next(iter(tables.values()))
+        df = next(iter(tables.values()))
+
+        if self._keys is not None:
+            if isinstance(self._keys, str):
+                key = self._keys
+            elif name in self._keys:
+                key = self._keys[name]
+            else:
+                key = None
+            if key is not None:
+                df = df.set_index(key)
+
+        # Make all columns lower case
+        df.columns = df.columns.str.lower()
+        if df.index is not None and df.index.names is not None:
+            df.index.names = [
+                n.lower() if n is not None else None for n in df.index.names
+            ]
+
+        return df
 
     @to_chunked
     def ingest(self, name, **tables: LazyChunk):
-        return self._process_chunk({name: table() for name, table in tables.items()})
-    
+        return self._process_chunk(name, {n: table() for n, table in tables.items()})
+
     def keys(self, **tables: LazyChunk) -> "pd.DataFrame":
         """Returns a DataFrame containing only the index column of the first table."""
         assert len(tables) == 1
@@ -67,10 +93,15 @@ class ConsumerExpendituresDataset(RfelDataset):
                 "EXPENDITURES",
             ]
         }
+        keys = {
+            "households": "HOUSEHOLD_ID",
+            "expenditures": "EXPENDITURE_ID",
+        }
         super().__init__(
             short_name="ce",
             name="consumer_expenditures",
             db="ConsumerExpenditures",
+            keys=keys,
             tables=tables,
             **kwargs,
         )
@@ -78,25 +109,32 @@ class ConsumerExpendituresDataset(RfelDataset):
 
 class StudentLoanDataset(RfelDataset):
     def __init__(self, **kwargs) -> None:
-        tables = {
-            k.lower(): [k]
-            for k in [
-                "person",
-                "bool",
-                "disabled",
-                "enlist",
-                "enrolled",
-                "filed_for_bankrupcy",
-                "longest_absense_from_school",
-                "male",
-                "no_payment_due",
-                "unemployed",
-            ]
+        all_tables = [
+            "person",
+            "bool",
+            "disabled",
+            "enlist",
+            "enrolled",
+            "filed_for_bankrupcy",
+            "longest_absense_from_school",
+            "male",
+            "no_payment_due",
+            "unemployed",
+        ]
+
+        tables = {k.lower(): [k] for k in all_tables}
+
+        keys = {
+            **{t.lower(): "name" for t in all_tables},
+            "enlist": ["name", "organ"],
+            "enrolled": ["name", "school"],
         }
+
         super().__init__(
             short_name="sl",
             name="student_loan",
             db="Student_loan",
             tables=tables,
+            keys=keys,
             **kwargs,
         )
