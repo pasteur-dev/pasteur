9a49f33a94eedee614b821995fbb4e12ce725120
Antheas Kapenekakis <git@antheas.dev>
2026-05-13 20:29:53 +0200

    tone down atol to converge more

--- src/pasteur/graph/mirror_descent.py
+++ src/pasteur/graph/mirror_descent.py
@@ -36,7 +36,7 @@ MIRROR_DESCENT_DEFAULT: MirrorDescentParams = {
     "lr": 1,
     "max_iters": 10_000,
     "ptol": 2e-4,
-    "atol": 1e-6,
+    "atol": 1e-8,
     "patience": 50,
     "device": "auto",
     "compile": False,
