6f40996e7ae804e48db75bd17fd461f47890a45b
Antheas Kapenekakis <git@antheas.dev>
2026-05-16 16:41:43 +0200

    fix multiple runs with adult having broken aggregate metrics

--- src/pasteur/extras/views/adult/__init__.py
+++ src/pasteur/extras/views/adult/__init__.py
@@ -5,3 +5,6 @@ class TabAdultView(TabularView):
     name = "tab_adult"
     dataset = "adult"
     parameters = get_relative_fn("parameters.yml")
+    # Adult is too small and when running multiple shuffles we end up
+    # with different transformer mappings which breaks distr metrics
+    fit_global = True
